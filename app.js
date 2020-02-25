0a272a2e138f52ecc4238f603bbcaa4469f563bf
Abhishek <48199959+Abhirek4874@users.noreply.github.com>
2020-02-26 00:47:48 +0530

    Add files via upload
    Added new CPU mode with advanced movements

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
+var emoji = require("node-emoji");
 let port = process.env.PORT || 5000;
 
 // set
@@ -8,19 +9,28 @@ app.set("view enginer", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
-
-app.post("/start-game", (req, res) => {
-  const userChoice = req.body.input;
-  res.render("game.ejs", {
-    player1name: req.body.player1name,
-    player1sign: "X",
-    player2name: req.body.player2name,
-    player2sign: "O"
+  res.render("index.ejs", {
+    heart: emoji.get("heart"),
+    brain: emoji.get("brain")
   });
 });
+app.post("/start-game", (req, res) => {
+  const userChoice = req.body.input;
+  let firstPlayer = req.body.player1name;
+  let secondPlayer = req.body.player2name;
 
-//8 winning moves are there
+  if (req.body.cpu !== undefined) {
+    firstPlayer = "Player";
+    secondPlayer = "Cpu";
+  }
 
+  res.render("game.ejs", {
+    player1name: firstPlayer,
+    player1sign: emoji.random().emoji,
+    player2name: secondPlayer,
+    player2sign: emoji.random().emoji,
+    radioPlayer: req.body.player,
+    radioCpu: req.body.cpu
+  });
+});
 app.listen(port);
